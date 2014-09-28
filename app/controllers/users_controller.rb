ff286c74f9435d8e93757b28aacd7860d40a117d
Sahil Dhankhar <sahil1345@gmail.com>
2014-09-28 10:09:11 +0530

    Fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,17 @@
 class UsersController < ApplicationController
-
-	def index
-
-		temp = Yummly.search(params[:search])
-		@result = temp.
-		collect { |recipe| Yummly.find(recipe.id) }.take(3)
-
-		@list = @result.collect {|x| x.id}
-		# @list[0]
-		#a = params[:id]
-		a = @list[0]
-		@recipe = Yummly.find(a)
-
-		
-		
-	end
+  def index
+    @search = params[:search]
+    unless @search.blank?
+      temp = Yummly.search(params[:search])
+      @result = temp.to_a[0..2].collect { |recipe| Yummly.find(recipe.id) }
+      @list = @result.collect {|x| x.id}
+      a = @list[0]
+      @recipe = Yummly.find(a)
+    else
+      @result = []
+      @list = []
+      @recipe = nil
+    end
+  end
 
 end
