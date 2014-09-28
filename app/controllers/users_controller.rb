3b5a42a04b8255d198b9be08352abfb0a9b10082
Sahil Dhankhar <sahil1345@gmail.com>
2014-09-28 10:19:25 +0530

    Optimization and more fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,6 @@ class UsersController < ApplicationController
     unless @search.blank?
       temp = Yummly.search(params[:search])
       @result = temp.to_a[0..2].collect { |recipe| Yummly.find(recipe.id) }
-      @list = @result.collect {|x| x.id}
-      a = @list[0]
-      @recipe = Yummly.find(a)
     else
       @result = []
       @list = []
